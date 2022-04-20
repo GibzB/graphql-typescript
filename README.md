3b75c06e67348d7333e61c7f3da4762bb617e8a8
RGibesh <ralphgibendi01@gmail.com>
2022-04-20 22:13:23 +0300

    final

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# graphql-typescript
+# Graphql-Typescript
 First attempt in coding using Typescript &amp; GraphQL
+
+## GraphQL
+GraphQL is another method for managing APIs. It's an alternative to Rest APIs that allows you to request "only the data you need". This helps reduce the amount of data that needs to be sent to the client from the server.
+## TypeScript
+TypeScript is a superset of JavaScript that compiles to JavaScript for production. It's like JavaScript, but with powers – type powers.
+TypeScript helps you build typed applications that help you avoid static type errors in those apps and make predictable code.
+TypeScript helps us build more predictable JavaScript code during development through type-checking
+
+### TypeGraphQL
+The goal of TypeGraphQL is to make it seamless to enjoy static typing in your resolvers and create your schemas from one place.
+#### Resolver decorator
+ Decorates the class as an object with many query and mutation resolve methods. The beauty here is we're defining the queries and mutations and the resolve methods in the same class.
+
+#### Query decorator
+Tells GraphQL that this is a query and the respective resolve method
+
+#### Mutation decorator
+Tells GraphQL that this is a mutation and the respective resolve method
+
+#### Arg decorator
+Tells GraphQL that this argument is a GraphQL argument for the resolver.
+
+## Test App
+Here i run the GraphiQL playgroud on my local env on http://localhost:8000/graphql with the query 
+
+[{
+  getUsers{
+    id
+    name
+    email
+  }
+}]
+
+Output is as below:
+
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { buildSchema } from "graphql"
 import express from "express"
 import { graphqlHTTP } from "express-graphql"
 
+/**users list */
 const users = [
-    { id: 1, name: "John Doe", email: "johndoe@gmail.com" },
-    { id: 2, name: "Jane Doe", email: "janedoe@gmail.com" },
-    { id: 3, name: "Mike Doe", email: "mikedoe@gmail.com" },
+    { id: 1, name: "King David", email: "kingdavid@ymail.com" },
+    { id: 2, name: "Queen Elizabeth", email: "queenelizabeth@gmail.com" },
+    { id: 3, name: "Sultan Jazeer", email: "sultaanjazeer@hotmail.com" },
 ]
-
+/** graphQLschema */
 const schema = buildSchema(`
     input UserInput {
         email: String!
@@ -31,3 +32,61 @@ const schema = buildSchema(`
         getUsers: [User]
     }
 `)
+
+type User = {
+    id: number
+    name: string
+    email: string
+}
+
+type UserInput = Pick<User, "email" | "name">
+
+/**Resolvers */
+const getUser = (args: { id: number }): User | undefined =>
+    users.find(u => u.id === args.id)
+
+const getUsers = (): User[] => users
+
+const createUser = (args: { input: UserInput }): User => {
+    const user = {
+        id: users.length + 1,
+        ...args.input,
+    }
+    users.push(user)
+
+    return user
+}
+
+const updateUser = (args: { user: User }): User => {
+    const index = users.findIndex(u => u.id === args.user.id)
+    const targetUser = users[index]
+
+    if (targetUser) users[index] = args.user
+
+    return targetUser
+}
+
+const root = {
+    getUser,
+    getUsers,
+    createUser,
+    updateUser,
+}
+
+/** express route and server */
+const app = express()
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+)
+
+const PORT = 8000
+
+app.listen(PORT)
+
+console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`)
